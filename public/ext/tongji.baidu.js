ccce6f705f4cacf9657293fd8ab2aa1ec8250f3c
why2fly <why2fly@aliyun.com>
2020-08-23 19:52:26 +0800

    加入百度统计

---
+++ public/ext/tongji.baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?92f89dc61725ffe8d6bc4b1225569875";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/getlist', async function (req, res) {
 	res.end(JSON.stringify(data));
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(122, function () {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("应用实例，访问地址为 http://%s:%s", host, port)
